a0d1d72f2be5f4f4eda9b27d8a49e553940efac8
Miguel Bento <miguelbento2000@gmail.com>
2024-02-12 17:23:15 +0100

    Strings

--- bash_scripting/strings.sh
+++ bash_scripting/strings.sh
@@ -14,6 +14,26 @@ expr index "$str" "$substr" # Finds the index in str of of any char in the subst
 ## EXTRACTING STR #################
 ###################################
 
+echo ${str:3}
+
+
+
+
+
+DATARECORD="last=Paulo,first=Joao,city=Toulouse"
+
+
+COMMA1=$(expr index "$DATARECORD" ",")
+
+echo $COMMA1
+
+CHOP1FIELD=${DATARECORD:$COMMA1}
+
+echo $CHOP1FIELD
+
+COMMA2=$(expr index "$CHOP1FIELD" ",")
+
+echo $COMMA2
 
 
 
---
+++ bash_scripting/strings_exercise.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+ISAY="Don't think about what can happen in a month. Don't think about what can happen in a year. Just focus on the 24 hours in front of you."
+ISAY=${ISAY[@]/snow/foot}
+ISAY=${ISAY[@]//snow/}
+ISAY=${ISAY[@]/finding/getting}
+ISAY=${expr index "$ISAY" "wet"}
+CHANGE4=${ISAY[@]:0:IDX_WISAY
